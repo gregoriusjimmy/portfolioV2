86a83028454905ea79e32cd4db1da568ca6510a0
gregoriusjimmy <gregoriusjimmy@gmail.com>
2019-11-13 17:39:34 +0700

    finishing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 $(function() {
   $(document).scroll(function() {
-    var $nav = $('.navbar')
-    $nav.toggleClass('navbar-scrolled', $(this).scrollTop() > $nav.height())
-  })
-})
+    var $nav = $(".navbar");
+    $nav.toggleClass("navbar-scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
